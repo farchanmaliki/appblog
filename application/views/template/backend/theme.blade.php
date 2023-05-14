769246f505896875f1880a029d7e55f8472c59ba
farchanmaliki <farchanmaliki@gmail.com>
2023-05-14 20:56:18 +0700

    edit fitur profile

--- application/views/template/backend/theme.blade.php
+++ application/views/template/backend/theme.blade.php
@@ -16,6 +16,7 @@
     
 
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
 
     <!-- Favicons -->
 <!-- <link rel="apple-touch-icon" href="/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
@@ -186,6 +187,12 @@
             <a class="nav-link" href="{{ site_url('backend/dashboard/table') }}">
               <span data-feather="file" class="align-text-bottom"></span>
               Table
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{ site_url('backend/user/profile') }}">
+            <i class="bi bi-person-circle" style= "margin-right: 4px;"></i>
+              Update Profile
             </a>
           </li>
         </ul>
